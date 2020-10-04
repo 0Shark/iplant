2dc30fe85af1cb5e4a6e95663083004170f0d57d
0Shark <47896217+0Shark@users.noreply.github.com>
2020-10-04 10:06:22 +0200

    Added JS related to donate, profile

---
+++ iPlant/js/donate-db.js
@@ -0,0 +1,15 @@
+
+$("#check .next").on('click', function(){
+  $("#check").hide("slide", { easing: "easeInQuart", direction: "left" }, 700, function(){
+    $(".processing").fadeIn(1500, function(){
+      $(".progress").animate({ width: "14em" }, 3500, "easeInOutCirc", function() {
+        $( ".mask" ).hide("slide", { easing: "easeInQuart", direction: "right" }, 400);
+      });
+    });
+  });
+  
+	setTimeout(function() {
+		$('.processing .message, .outer').fadeOut();
+  	$("#confirm").addClass('animated fadeInUp');
+	}, 6250);
+});
---
+++ iPlant/js/profilepic.js
@@ -0,0 +1,12 @@
+function getRandomImage() {
+var images = ['img/tree1.png', 'img/tree2.png', 'img/tree3.png'];
+var image = images[Math.floor(Math.random()*images.length)];
+ 
+return image;
+}
+ 
+function displayRandomImage() {
+var htmlImage = document.getElementById("profilepic");
+htmlImage.src = getRandomImage();
+}
+displayRandomImage();
